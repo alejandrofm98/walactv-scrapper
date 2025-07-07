84b763a341f1cf3de50c4001358305fe3513f8a5
alejandrofm98 <alejandrofmayor98@gmail.com>
2025-07-07 20:06:42 +0200

    feat: Habilitar modo headless en el WebDriver para optimizar ejecución en scrapper

--- scripts/newScrapper.py
+++ scripts/newScrapper.py
@@ -144,7 +144,7 @@ class NewScrapper:
   def _setup_chrome_driver(self):
       """Configura y retorna una instancia de Chrome WebDriver."""
       chrome_options = Options()
-      # chrome_options.add_argument("--headless")
+      chrome_options.add_argument("--headless")
       chrome_options.add_argument('--no-sandbox')
       chrome_options.add_argument('--disable-dev-shm-usage')
       chrome_options.add_argument('--disable-gpu')
