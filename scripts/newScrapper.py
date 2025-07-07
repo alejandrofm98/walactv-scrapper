2e9363f2cbaaef4764da15abf3c1fe5b6b544450
alejandrofm98 <alejandrofmayor98@gmail.com>
2025-07-07 23:55:59 +0200

    feat: Configurar user-agent en el WebDriver para mejorar compatibilidad y evitar detección

--- scripts/newScrapper.py
+++ scripts/newScrapper.py
@@ -150,6 +150,7 @@ class NewScrapper:
       chrome_options.add_argument('--disable-gpu')
       chrome_options.add_argument('--window-size=1920,1080')
       chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+      chrome_options.add_argument('--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36')
       db = Database("configNewScrapper", 'proxy', None)
       proxy = db.get_doc_firebase().to_dict()
 
