3e7db2ed983a8a30851bf2e609a6323dce83b441
alejandrofm98 <alejandrofmayor98@gmail.com>
2025-07-09 19:55:54 +0200

    refactor: Añadir configuración de opciones y servicio en WebDriver para soporte headless y rutas personalizadas

--- scripts/prueba.py
+++ scripts/prueba.py
@@ -1,4 +1,6 @@
 from seleniumwire import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 class Prueba:
   def __init__(self):
@@ -8,6 +10,17 @@ class Prueba:
 
   def prueba(self):
     print("hola")
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')  # Optional for no GUI
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    CHROMIUM_PATH = "/usr/bin/chromium-browser"
+    CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
+
+    options.binary_location = CHROMIUM_PATH
+    service = Service(CHROMEDRIVER_PATH)
+
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get(self.url + self.url_agenda)
     print(driver.page_source)
