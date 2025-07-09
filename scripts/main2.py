ae12bef35b90cab1983cc0a8a9d5fc79fa620f82
alejandrofm98 <alejandrofmayor98@gmail.com>
2025-07-09 23:48:12 +0200

    refactor: Reemplazar lógica comentada en `main2.py` con llamada a nueva clase `Prueba` y añadir implementación inicial en `prueba.py`

--- scripts/main2.py
+++ scripts/main2.py
@@ -1,17 +1,22 @@
 import newScrapper
 from newScrapper import guarda_partidos
 import openRouter
+import prueba
 
 if __name__ == '__main__':
 
   try:
-    tv_libre = newScrapper.NewScrapper()
-    tv_libre.obtener_titulo_eventos()
-    eventos = tv_libre.process_streams()
+    # tv_libre = newScrapper.NewScrapper()
+    # tv_libre.obtener_titulo_eventos()
+    # eventos = tv_libre.process_streams()
+    #
+    # open_router = openRouter.OpenRouter(events=eventos["eventos"])
+    # eventos["eventos"] = open_router.get_category_events()
+    # guarda_partidos(eventos)
+    # print(eventos)
+    prueba = prueba.Prueba()
+    prueba.prueba()
 
-    open_router = openRouter.OpenRouter(events=eventos["eventos"])
-    eventos["eventos"] = open_router.get_category_events()
-    guarda_partidos(eventos)
-
+  # Optional: stop OpenVPN
   except Exception as e:
     print(f"An error occurred: {e}")
---
+++ scripts/prueba.py
@@ -0,0 +1,54 @@
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+from database import Database
+import platform
+import requests
+
+
+def is_arm():
+  return platform.machine().startswith(
+    "aarch") or "arm" in platform.machine().lower()
+
+class Prueba:
+  def __init__(self):
+    self.guarda_eventos = None
+    self.url = "https://tvlibreonline.org"
+    self.url_agenda = "/agenda/"
+    db = Database("configNewScrapper", 'proxy', None)
+    proxy = db.get_doc_firebase().to_dict()
+
+    proxy_ip = proxy.get("proxy_ip")
+    proxy_port = proxy.get("proxy_port")
+    proxy_user = proxy.get("proxy_user")
+    proxy_pass = proxy.get("proxy_pass")
+
+    self.seleniumwire_options = {
+      "proxy": {
+        'http': 'http://' + proxy_user + ':' + proxy_pass + '@' + proxy_ip + ':' + proxy_port
+      }
+    }
+
+
+  def get_driver(self):
+    options = Options()
+    options.add_argument('--headless')  # Optional for no GUI
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    if is_arm():
+      # VPS or ARM system
+      options.binary_location = "/usr/bin/chromium-browser"
+      return webdriver.Chrome(
+          service=Service("/usr/bin/chromedriver"),
+          options=options
+      )
+    else:
+      # Desktop or x86 (assuming Chrome is installed and in PATH)
+      return webdriver.Chrome(options=options, seleniumwire_options=self.seleniumwire_options)
+  def prueba(self):
+    print("hola")
+    driver = self.get_driver()
+    driver.get(self.url + self.url_agenda)
+    print(driver.page_source)
