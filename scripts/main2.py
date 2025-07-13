040e870bc49cfb225dbdc1902c2f74ad2cad4049
alejandrofm98 <alejandrofmayor98@gmail.com>
2025-07-13 13:47:44 +0200

    refactor: Reactivar lógica principal en `main2.py` y actualizar referencia de módulo a `prueba2`

--- scripts/main2.py
+++ scripts/main2.py
@@ -1,22 +1,22 @@
 import newScrapper
 from newScrapper import guarda_partidos
 import openRouter
-import prueba
+import prueba2
 import openvpn
 
 if __name__ == '__main__':
   OpenVPN = openvpn.OpenVpn()
   try:
     # OpenVPN.start_openvpn()
-    # tv_libre = newScrapper.NewScrapper()
-    # tv_libre.obtener_titulo_eventos()
-    # eventos = tv_libre.process_streams()
-    # open_router = openRouter.OpenRouter(events=eventos["eventos"])
-    # eventos["eventos"] = open_router.get_category_events()
-    # guarda_partidos(eventos)
-    # print(eventos)
-    prueba = prueba.Prueba()
-    prueba.prueba2()
+    tv_libre = newScrapper.NewScrapper()
+    tv_libre.obtener_titulo_eventos()
+    eventos = tv_libre.process_streams()
+    open_router = openRouter.OpenRouter(events=eventos["eventos"])
+    eventos["eventos"] = open_router.get_category_events()
+    guarda_partidos(eventos)
+    print(eventos)
+    # prueba = prueba2.Prueba()
+    # prueba.prueba2()
   # Optional: stop OpenVPN
   except Exception as e:
     print(f"An error occurred1: {e.with_traceback(e.__traceback__)}")
